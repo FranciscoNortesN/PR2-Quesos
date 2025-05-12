1ab64ae2dad80103fdd74b698c510018f2771626
VicenteBurdeus <vbursan@upv.edu.es>
2025-05-12 20:23:46 +0200

    añadido mqtt para la web

--- ScripCreacionV1.sql
+++ ScripCreacionV1.sql
@@ -1,11 +1,10 @@
--- Nombre: Scrip Creacion.sql
+-- Nombre: Script Creacion.sql
 -- Script Creacion.sql para PostgreSQL V0.1 Proyecto Queso
 -- VICENTE BURDEUS SANCHEZ
 -- Fecha: 2023-10-05
 -- HORA : 17:08
 
-
---Almacens-------------------------------------------------------------- 
+-- Almacenes --------------------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS almacen (
     "id_almacen" char(10) PRIMARY KEY,
@@ -14,17 +13,17 @@ CREATE TABLE IF NOT EXISTS almacen (
     "ultima_revision" date
 );
 
-CREATE TABLE if NOT EXISTS estanteria (
+CREATE TABLE IF NOT EXISTS estanteria (
     "id_estanteria" char(10) PRIMARY KEY,
     "localizacion" char(10) NOT NULL,
     "tipo_queso" char(10) NOT NULL,
-    "fecha_inicio" date NOT NULL, --solo el dia de instalacion
+    "fecha_inicio" date NOT NULL, -- solo el día de instalación
     "id_almacen" char(10),
     CONSTRAINT id_almacen_estanteria_fk FOREIGN KEY ("id_almacen") REFERENCES almacen 
     ON DELETE CASCADE ON UPDATE CASCADE    
 );
 
---Nodos de temperatura y humedad-----------------------------------------------
+-- Nodos de temperatura y humedad -----------------------------------------------
 
 CREATE TABLE IF NOT EXISTS nodotemperaturainfo (
     "id_nodo" char(10) PRIMARY KEY,
@@ -35,7 +34,7 @@ CREATE TABLE IF NOT EXISTS nodotemperaturainfo (
     ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS ntdato(
+CREATE TABLE IF NOT EXISTS ntdato (
     "id_nodo" char(10) NOT NULL,
     "temperatura" real NOT NULL,
     "humedad" integer NOT NULL,
@@ -45,7 +44,8 @@ CREATE TABLE IF NOT EXISTS ntdato(
     ON DELETE CASCADE ON UPDATE CASCADE
 );
 
---Info de los quesos---------------------------------------------------------------
+-- Info de los quesos ---------------------------------------------------------------
+
 CREATE TABLE IF NOT EXISTS queso (
     "id_queso" char(10) PRIMARY KEY,
     "localizacion" char(10) NOT NULL,
@@ -69,7 +69,7 @@ CREATE TABLE IF NOT EXISTS lote (
     ON DELETE CASCADE ON UPDATE CASCADE
 );
 
---Clientes---------------------------------------------------------------
+-- Clientes ---------------------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS cliente (
     "id_cliente" char(10) PRIMARY KEY,
@@ -77,7 +77,7 @@ CREATE TABLE IF NOT EXISTS cliente (
     "tipo_cliente" char(10) NOT NULL,
     "informacion_contacto" char(100) NOT NULL
 );
-	
+
 CREATE TABLE IF NOT EXISTS pedido (
     "id_identificador" char(10) PRIMARY KEY,
     "cliente" char(10) NOT NULL,
@@ -110,8 +110,8 @@ CREATE TABLE IF NOT EXISTS pedidoinfo (
     ON DELETE CASCADE ON UPDATE CASCADE
 );
 
---Proveedores------------------------------------------------------------
-			
+-- Proveedores ------------------------------------------------------------
+
 CREATE TABLE IF NOT EXISTS proveedor (
     "id_proveedor" char(10) PRIMARY KEY,
     "nombre" char(50) NOT NULL,
@@ -120,7 +120,7 @@ CREATE TABLE IF NOT EXISTS proveedor (
     "telefono" char(15) NOT NULL
 );
 
---agvs------------------------------------------------------------------
+-- AGVs ------------------------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS robotagvinfo (
     "id_robot" char(10) PRIMARY KEY,
@@ -128,20 +128,20 @@ CREATE TABLE IF NOT EXISTS robotagvinfo (
     "carga" integer
 );
 
-CREATE TABLE IF NOT EXISTS rutas(
+CREATE TABLE IF NOT EXISTS rutas (
     "id_robot" char(10) NOT NULL,
     "inicio" char(10) NOT NULL,
     "destino" char(10) NOT NULL,
     "fecha" timestamp NOT NULL DEFAULT NOW(),
     CONSTRAINT id_robot_rutas_fk FOREIGN KEY ("id_robot") REFERENCES robotagvinfo
     ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT vnn_inicio_rutas_fk FOREIGN KEY ("vnn_inicio") REFERENCES estanteria
+    CONSTRAINT inicio_rutas_fk FOREIGN KEY ("inicio") REFERENCES estanteria
     ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT vnn_destino_rutas_fk FOREIGN KEY ("vnn_destino") REFERENCES estanteria
+    CONSTRAINT destino_rutas_fk FOREIGN KEY ("destino") REFERENCES estanteria
     ON DELETE CASCADE ON UPDATE CASCADE
-)
+);
 
---Usuarios---------------------------------------------------------------
+-- Usuarios ---------------------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS userinfo (
     "id_usuario" char(10) PRIMARY KEY,
@@ -150,19 +150,19 @@ CREATE TABLE IF NOT EXISTS userinfo (
     "tipo_usuario" char(15) NOT NULL CHECK ("tipo_usuario" IN ('admin', 'operador', 'mantenimiento', 'supervisor')),
     "correo" char(50) NOT NULL,
     "telefono" char(15) NOT NULL,
-    "contrasena" char(50) NOT NULL
-    "dataid" char(100) NOT NULL,
+    "contrasena" char(50) NOT NULL,
+    "dataid" char(100) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS userlogin (
     "id_usuario" char(10) NOT NULL,
     "fecha_login" timestamp NOT NULL DEFAULT NOW(),
     "ip" char(15) NOT NULL,
-    CONSTRAINT id_usuario_userslogin_fk FOREIGN KEY ("id_usuario") REFERENCES USERS
+    CONSTRAINT id_usuario_userslogin_fk FOREIGN KEY ("id_usuario") REFERENCES userinfo
     ON DELETE CASCADE ON UPDATE CASCADE
 );
 
---Preguntas-Web-------------------------------------------------------------
+-- Preguntas-Web -------------------------------------------------------------
 
 CREATE SEQUENCE formulario_seq
     START WITH 1
@@ -176,6 +176,28 @@ CREATE TABLE IF NOT EXISTS formularios (
     "nombre" char(50),
     "correo" char(50) NOT NULL,
     "mensaje" char(500) NOT NULL,
-    "fecha" timestamp NOT NULL DEFAULT NOW(),
+    "fecha" timestamp NOT NULL DEFAULT NOW()
 );
 
+--Datos MQTT -------------------------------------------------------------
+
+
+CREATE SEQUENCE topic_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE TABLE IF NOT EXISTS mqtt_topics (
+    "id_topic" integer PRIMARY KEY DEFAULT nextval('topic_seq'),
+    "topic" char(50) NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS mqtt_datos (
+    "id_dato" integer NOT NULL,
+    "fecha" timestamp NOT NULL DEFAULT NOW(),
+    "payload" char(500) NOT NULL,
+    CONSTRAINT id_topic_mqtt_datos_fk FOREIGN KEY ("id_topic") REFERENCES mqtt_topics
+    ON DELETE CASCADE ON UPDATE CASCADE
+);
